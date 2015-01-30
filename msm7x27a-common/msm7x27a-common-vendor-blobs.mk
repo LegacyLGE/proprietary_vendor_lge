d165d2e7ecdef22199a16022a585d114f72b93bc
aidasaidas75 <aidaslukosius75@gmail.com>
2015-01-30 14:33:30 +0200

    msm7x27a-common:Update Adreno Blobs

--- msm7x27a-common/msm7x27a-common-vendor-blobs.mk
+++ msm7x27a-common/msm7x27a-common-vendor-blobs.mk
@@ -19,14 +19,7 @@ PRODUCT_COPY_FILES += \
     vendor/lge/msm7x27a-common/proprietary/bin/mm-qcamera-daemon:system/bin/mm-qcamera-daemon \
     vendor/lge/msm7x27a-common/proprietary/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
     vendor/lge/msm7x27a-common/proprietary/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
-    vendor/lge/msm7x27a-common/proprietary/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
-    vendor/lge/msm7x27a-common/proprietary/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
-    vendor/lge/msm7x27a-common/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
-    vendor/lge/msm7x27a-common/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
-    vendor/lge/msm7x27a-common/proprietary/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
-    vendor/lge/msm7x27a-common/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
     vendor/lge/msm7x27a-common/proprietary/lib/libaudcal.so:system/lib/libaudcal.so \
-    vendor/lge/msm7x27a-common/proprietary/lib/libC2D2.so:system/lib/libC2D2.so \
     vendor/lge/msm7x27a-common/proprietary/lib/libdivxdrmdecrypt.so:system/lib/libdivxdrmdecrypt.so \
     vendor/lge/msm7x27a-common/proprietary/lib/libDivxDrm.so:system/lib/libDivxDrm.so \
     vendor/lge/msm7x27a-common/proprietary/lib/libExtendedExtractor.so:system/lib/libExtendedExtractor.so \
@@ -34,12 +27,8 @@ PRODUCT_COPY_FILES += \
     vendor/lge/msm7x27a-common/proprietary/lib/libI420colorconvert.so:system/lib/libI420colorconvert.so \
     vendor/lge/msm7x27a-common/proprietary/lib/libimage-jpeg-enc-omx-comp.so:system/lib/libimage-jpeg-enc-omx-comp.so \
     vendor/lge/msm7x27a-common/proprietary/lib/libimage-omx-common.so:system/lib/libimage-omx-common.so \
-    vendor/lge/msm7x27a-common/proprietary/lib/libgsl.so:system/lib/libgsl.so \
-    vendor/lge/msm7x27a-common/proprietary/lib/libmmcamera_faceproc.so:system/lib/libmmcamera_faceproc.so \
-    vendor/lge/msm7x27a-common/proprietary/lib/libmmcamera_frameproc.so:system/lib/libmmcamera_frameproc.so \
     vendor/lge/msm7x27a-common/proprietary/lib/libmmcamera_hdr_lib.so:system/lib/libmmcamera_hdr_lib.so \
     vendor/lge/msm7x27a-common/proprietary/lib/libmmcamera_image_stab.so:system/lib/libmmcamera_image_stab.so \
-    vendor/lge/msm7x27a-common/proprietary/lib/libmmcamera_statsproc31.so:system/lib/libmmcamera_statsproc31.so \
     vendor/lge/msm7x27a-common/proprietary/lib/libmmcamera_wavelet_lib.so:system/lib/libmmcamera_wavelet_lib.so \
     vendor/lge/msm7x27a-common/proprietary/lib/libmm-adspsvc.so:system/lib/libmm-adspsvc.so \
     vendor/lge/msm7x27a-common/proprietary/lib/libmm-audio-resampler.so:system/lib/libmm-audio-resampler.so \
@@ -49,10 +38,6 @@ PRODUCT_COPY_FILES += \
     vendor/lge/msm7x27a-common/proprietary/lib/libmmosal.so:system/lib/libmmosal.so \
     vendor/lge/msm7x27a-common/proprietary/lib/libmmparser.so:system/lib/libmmparser.so \
     vendor/lge/msm7x27a-common/proprietary/lib/libmmstillomx.so:system/lib/libmmstillomx.so \
-    vendor/lge/msm7x27a-common/proprietary/lib/libmorpho_memory_allocator.so:system/lib/libmorpho_memory_allocator.so \
-    vendor/lge/msm7x27a-common/proprietary/lib/libmorpho_noise_reduction.so:system/lib/libmorpho_noise_reduction.so \
-    vendor/lge/msm7x27a-common/proprietary/lib/libmorpho_panorama_gp.so:system/lib/libmorpho_panorama_gp.so \
-    vendor/lge/msm7x27a-common/proprietary/lib/libmorpho_panorama_wa_viewer.so:system/lib/libmorpho_panorama_wa_viewer.so \
     vendor/lge/msm7x27a-common/proprietary/lib/libOmxAacDec.so:system/lib/libOmxAacDec.so \
     vendor/lge/msm7x27a-common/proprietary/lib/libOmxAc3HwDec.so:system/lib/libOmxAc3HwDec.so \
     vendor/lge/msm7x27a-common/proprietary/lib/libOmxAdpcmDec.so:system/lib/libOmxAdpcmDec.so \
@@ -74,9 +59,22 @@ PRODUCT_COPY_FILES += \
     vendor/lge/msm7x27a-common/proprietary/lib/libOmxVp8Dec.so:system/lib/libOmxVp8Dec.so \
     vendor/lge/msm7x27a-common/proprietary/lib/libOmxWmaDec.so:system/lib/libOmxWmaDec.so \
     vendor/lge/msm7x27a-common/proprietary/lib/libOmxWmvDec.so:system/lib/libOmxWmvDec.so \
-    vendor/lge/msm7x27a-common/proprietary/lib/libOpenVG.so:system/lib/libOpenVG.so \
-    vendor/lge/msm7x27a-common/proprietary/lib/libOpenCL.so:system/lib/libOpenCL.so \
-    vendor/lge/msm7x27a-common/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
     vendor/lge/msm7x27a-common/proprietary/lib/libqc-opt.so:system/lib/libqc-opt.so \
     vendor/lge/msm7x27a-common/proprietary/lib/libSHIMDivxDrm.so:system/lib/libSHIMDivxDrm.so \
-    vendor/lge/msm7x27a-common/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so
+# 3D(New adreno Blobs)
+PRODUCT_COPY_FILES += \
+    vendor/lge/msm7x27a-common/proprietary/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
+    vendor/lge/msm7x27a-common/proprietary/lib/libgsl.so:system/vendor/lib/libgsl.so \
+    vendor/lge/msm7x27a-common/proprietary/lib/libllvm-arm.so:system/vendor/lib/libllvm-arm.so \
+    vendor/lge/msm7x27a-common/proprietary/lib/libOpenCL.so:system/vendor/lib/libOpenCL.so \
+    vendor/lge/msm7x27a-common/proprietary/lib/libOpenVG.so:system/vendorlib/libOpenVG.so \
+    vendor/lge/msm7x27a-common/proprietary/lib/libsc-a2xx.so:system/vendor/lib/libsc-a2xx.so \
+    vendor/lge/msm7x27a-common/proprietary/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
+    vendor/lge/msm7x27a-common/proprietary/lib/libc2d2_z180.so:system/vendor/lib/libc2d2_z180.so \
+    vendor/lge/msm7x27a-common/proprietary/lib/egl/eglsubAndroid.so:system/vendor/lib/egl/eglsubAndroid.so \
+    vendor/lge/msm7x27a-common/proprietary/lib/egl/libEGL_adreno.so:system/vendor/lib/egl/libEGL_adreno.so \
+    vendor/lge/msm7x27a-common/proprietary/lib/egl/libGLESv1_CM_adreno.so:system/vendor/lib/egl/libGLESv1_CM_adreno.so \
+    vendor/lge/msm7x27a-common/proprietary/lib/egl/libGLESv2_adreno.so:system/vendor/lib/egl/libGLESv2_adreno.so \
+    vendor/lge/msm7x27a-common/proprietary/lib/egl/libGLESv2S3D_adreno.so:system/vendor/lib/egl/libGLESv2S3D_adreno.so \
+    vendor/lge/msm7x27a-common/proprietary/lib/egl/libq3dtools_adreno.so:system/vendorlib/egl/libq3dtools_adreno.so 
+
